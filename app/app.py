7c7d9175b9f7aed43e401df5e230e23643e3b45e
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2025-12-27 00:05:49 +0000

    Flask application code

--- app/app.py
+++ app/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "CI/CD automation project is running successfully",
+        "status": "OK"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
